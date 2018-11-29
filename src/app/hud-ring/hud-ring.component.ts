e30d472099c7410b02bb63942ef8282c01addee9
Josue Zarzosa <josue.zarzosa@esailors.de>
2018-11-29 14:54:48 +0000

    add paralax

--- src/app/hud-ring/hud-ring.component.ts
+++ src/app/hud-ring/hud-ring.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {fromEvent} from 'rxjs';
 
 @Component({
@@ -8,12 +8,36 @@ import {fromEvent} from 'rxjs';
 })
 export class HudRingComponent implements OnInit {
 
-  constructor() {
+  constructor(private elRef: ElementRef) {
   }
 
   ngOnInit() {
     fromEvent(window, 'deviceorientation')
-      .subscribe(console.log);
+      .subscribe((event: DeviceOrientationEvent) => {
+
+        const beta = event.beta;
+        const offsetY1 = beta * 4;
+        const offsetY2 = beta * 3;
+        const offsetY3 = beta * 2;
+        const offsetY4 = beta;
+
+        const gamma = event.gamma * 0.5;
+        const offsetX1 = gamma * 4;
+        const offsetX2 = gamma * 3;
+        const offsetX3 = gamma * 2;
+        const offsetX4 = gamma;
+
+        this.elRef.nativeElement.style.setProperty('--ring-1-y-offset', offsetY1 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-2-y-offset', offsetY2 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-3-y-offset', offsetY3 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-4-y-offset', offsetY4 + 'px');
+
+        this.elRef.nativeElement.style.setProperty('--ring-1-x-offset', offsetX1 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-2-x-offset', offsetX2 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-3-x-offset', offsetX3 + 'px');
+        this.elRef.nativeElement.style.setProperty('--ring-4-x-offset', offsetX4 + 'px');
+
+      });
   }
 
 }
